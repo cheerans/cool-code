dcd634206aca33c03059334ffb25113c858a8319
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2020-06-19 23:03:51 -0400

    Update SQLInjectionScannerUtil.java

--- SQLInjectionScannerUtil.java
+++ SQLInjectionScannerUtil.java
@@ -8,30 +8,43 @@ import java.util.List;
 
 public class SQLInjectionScannerUtil {
 
-	public static String regExSQLInject = "(script )|(&lt;)|(&gt;)|(%3c)|(%3e)|(SLEEP )|(TRUNCATE )|(VERSION )|(JOIN )|(DROP )|(ORDER )|(SELECT )|(UPDATE )|(INSERT )|(DELETE )|(GRANT )|(REVOKE )|(UNION )|(&amp;lt;)|(&amp;gt;)|(;)";
+	public static String regExSQLInject = "(script )|(&lt;)|(&gt;)|(%3c)|(%3e)|(SLEEP )|(TRUNCATE )|(VERSION )|(JOIN )|(DROP )|(ORDER )|(SELECT )|(UPDATE )|(INSERT )|(DELETE )|(GRANT )|(REVOKE )|(UNION )|(OR )|(&amp;lt;)|(&amp;gt;)|(;)";
 	
 	public static String deInjectSQLInFieldValue(String sqlMaybeInjected) {		
 		return java.util.regex.Pattern.compile(regExSQLInject, java.util.regex.Pattern.CASE_INSENSITIVE).matcher(sqlMaybeInjected).replaceAll("");
 	}
 	
-	public static boolean checkSQLInjectionForForm(Object formBean) {
+	public static boolean checkSQLInjectionForForm(Object formBean, Object ... args) {
 		
 		boolean bSqlInjectionDetected = false;
+		List<String> exludeFields = null;
+		if(args.length > 0){
+			exludeFields = (List<String>) args[0];
+		}
 		if (null != formBean) {
 			List<Field> fields = getFieldsArray(formBean.getClass());
 			String fieldVal = "";
 			String modVal = "";
 			for (Field fld : fields) {
-				if (	(false == Modifier.isTransient(fld.getModifiers())) && 	
-				    	(false == Modifier.isFinal(fld.getModifiers())) && 	
-				    	(fld.getType().equals(String.class))) {
-					
+				if (		(false == Modifier.isTransient(fld.getModifiers()))
+					&& 	(false == Modifier.isFinal(fld.getModifiers()))
+					&& 	(fld.getType().equals(String.class))) {					
 					try {
-						fieldVal = fld.get(formBean).toString();
-						modVal = deInjectSQLInFieldValue(fieldVal);
-						if(false == fieldVal.equals(modVal)){				
-							bSqlInjectionDetected = true;								
-						}				
+						if(fld.getName().equals(ControllerConstants.CRUDAFIELDNAME)){
+							continue;
+						}else if( 	(null != exludeFields) &&
+								(true == exludeFields.contains(fld.getName()))){
+							continue;
+						}
+						else{
+							fld.setAccessible(true);							
+							fieldVal = fld.get(formBean).toString();
+							modVal = deInjectSQLInFieldValue(fieldVal);
+							if(false == fieldVal.equals(modVal)){
+								bSqlInjectionDetected = true;							
+							}
+							fld.setAccessible(false);
+						}
 					} catch (Exception e) {
 					}
 				}
